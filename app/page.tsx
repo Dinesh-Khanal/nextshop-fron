688ea5113e97e4f1057e830a52c7925fc30899f5
Dinesh-Khanal <dinesh_Khanal@hotmail.com>
2023-05-24 17:24:37 +0545

    heroicons/react installed and icon use in page, image of featured product fetched and placed

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { getProductById } from "@/lib/products";
 import Image from "next/image";
+import { ShoppingCartIcon } from "@heroicons/react/24/outline";
 
 export default async function Home() {
   const { product } = await getProductById("646ae56fae77a830b5e0309f");
@@ -14,6 +15,15 @@ export default async function Home() {
             magni repudiandae labore quibusdam vitae, ea aperiam at quae.
             Beatae, non. Dolor, voluptatem!
           </p>
+          <div className="flex gap-4 mt-4 items-center">
+            <button className="border border-white p-1 rounded">
+              Read more
+            </button>
+            <button className="btn-primary flex gap-1 items-center">
+              <ShoppingCartIcon className="h-6 w-6" />
+              <span>Add to cart</span>
+            </button>
+          </div>
         </div>
         <div className="w-full h-72 relative mr-8">
           {product && (
