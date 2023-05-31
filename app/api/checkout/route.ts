019af65f3b4012833411b6ee453d259852267076
Dinesh-Khanal <dinesh_Khanal@hotmail.com>
2023-05-31 22:24:48 +0545

    stripe payment added

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -1,4 +1,6 @@
 import { createOrder, getOrders } from "@/lib/orders";
+import Stripe from "stripe";
+import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
   const orders = await getOrders();
@@ -7,5 +9,32 @@ export async function GET(request: Request) {
 export async function POST(request: Request) {
   const body = await request.json();
   const order = await createOrder(body);
-  return new Response(JSON.stringify(order), { status: 200 });
+  //@ts-ignore
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+  const productPrice = 500;
+  const quantity = 1;
+  let line_items = [];
+  for (const product of body.products) {
+    line_items.push({
+      quantity,
+      price_data: {
+        currency: "USD",
+        product_data: { name: product },
+        unit_amount: quantity * productPrice * 100,
+        //unit_amount: quantity * productInfo.price * 100,
+      },
+    });
+  }
+
+  const session = await stripe.checkout.sessions.create({
+    line_items,
+    customer_email: body.email,
+    mode: "payment",
+    success_url: process.env.PUBLIC_URL + "/cart?success=1",
+    cancel_url: process.env.PUBLIC_URL + "/cart?canceled=1",
+    metadata: { orderId: order.order.id, test: "ok" },
+  });
+  return NextResponse.json(session.url);
+  //return new Response(JSON.stringify({ url: session.url }), { status: 200 });
 }
